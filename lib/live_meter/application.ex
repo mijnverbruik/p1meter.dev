9adabd57dc0ca179a446bdb86c14fef296aaa64f
Robin van der Vleuten <robin@webstronauts.com>
2026-07-04 09:30:10 +0200

    refactor: remove duplicate enabled-check child spec logic

--- lib/live_meter/application.ex
+++ lib/live_meter/application.ex
@@ -12,8 +12,8 @@ defmodule LiveMeter.Application do
       {DNSCluster, query: Application.get_env(:live_meter, :dns_cluster_query) || :ignore},
       {Phoenix.PubSub, name: LiveMeter.PubSub},
       LiveMeter.RateLimit,
-      tcp_server_spec(),
-      smart_meter_spec(),
+      {LiveMeter.TCPServer, Application.get_env(:live_meter, LiveMeter.TCPServer, [])},
+      {LiveMeter.SmartMeter, Application.get_env(:live_meter, LiveMeter.SmartMeter, [])},
       # Start to serve requests, typically the last entry
       LiveMeterWeb.Endpoint
     ]
@@ -22,28 +22,6 @@ defmodule LiveMeter.Application do
     # for other strategies and supported options
     opts = [strategy: :one_for_one, name: LiveMeter.Supervisor]
     Supervisor.start_link(children, opts)
-  end
-
-  defp tcp_server_spec do
-    maybe_enabled_spec(
-      LiveMeter.TCPServer,
-      Application.get_env(:live_meter, LiveMeter.TCPServer, [])
-    )
-  end
-
-  defp smart_meter_spec do
-    maybe_enabled_spec(
-      LiveMeter.SmartMeter,
-      Application.get_env(:live_meter, LiveMeter.SmartMeter, [])
-    )
-  end
-
-  defp maybe_enabled_spec(module, opts) do
-    if Keyword.get(opts, :enabled, true) do
-      {module, opts}
-    else
-      %{id: module, start: {Function, :identity, [:ignore]}}
-    end
   end
 
   # Tell Phoenix to update the endpoint configuration
